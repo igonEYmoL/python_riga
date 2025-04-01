3ad56f33430731d814196206c406bc1b4aa0b4a6
Nicolas FISCHER <nicolas.fischer@ccicampus.fr>
2025-04-01 21:23:41 +0300

    Refactor task.py pour encapsuler la logique de comparaison dans une fonction eval

--- task.py
+++ task.py
@@ -1,7 +1,10 @@
 # Write if ellese if x is larger than y Return True if YES
 x = int(input('Enter a number: '))
 y = int(input('Enter another number: '))
-if x > y:
-    print('True')
-else:
-    print('False')
+
+def eval(x, y):
+    if x > y:
+        return True
+    return False
+
+print(eval(x, y))
