692435e1228dc46e70dcc1a0b6e8802ead271ff8
Nicolas FISCHER <nicolas.fischer@ccicampus.fr>
2025-04-01 20:45:04 +0300

    add first code

--- variable.py
+++ variable.py
@@ -2,3 +2,19 @@ X = 10
 y = 'Marco'
 print(X)
 print(y)
+name = 'Nicolas'
+surname = 'Fischer'
+student = name + ' ' + surname
+print(student)
+a = 10
+b = 5
+c = a - b
+print(c)
+c = a + b
+print(c)
+c = a * b
+print(c)
+c = a / b
+print(c)
+
+# This is a comment
