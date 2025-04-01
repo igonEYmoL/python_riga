415aff53f95f573531aacd272761e1668c8ed61d
Nicolas FISCHER <nicolas.fischer@ccicampus.fr>
2025-04-01 21:28:02 +0300

    Modification de la fonction eval dans task.py pour inclure une nouvelle condition de comparaison avec une variable b

--- task.py
+++ task.py
@@ -1,10 +1,10 @@
 # Write if ellese if x is larger than y Return True if YES
 x = int(input('Enter a number: '))
 y = int(input('Enter another number: '))
-
-def eval(x, y):
-    if x > y:
+b = 100
+def eval(x, y, b):
+    if (x > y and x < b):
         return True
     return False
 
-print(eval(x, y))
+print(eval(x, y, b))
