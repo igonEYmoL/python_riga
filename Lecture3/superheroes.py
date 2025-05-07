0088983784842e88e1e109caf6bef8424d98d19f
Nicolas FISCHER <nicolas.fischer@ccicampus.fr>
2025-05-07 19:16:49 +0300

    Modification de la classe Superhero pour ajouter des attributs origin, frends et action_power, et mise à jour de la méthode use_power pour refléter ces changements.

--- Lecture3/superheroes.py
+++ Lecture3/superheroes.py
@@ -1,10 +1,14 @@
 class Superhero:
-    def __init__(self, name, power):
+    def __init__(self, name, power, origin, frends, action_power):
         self.name = name
         self.power = power
+        self.frends = frends
+        self.origin = origin
+        self.action_power = action_power
+        self.energy = 100
 
     def use_power(self):
-        print(f"{self.name} uses {self.power}!")
+        print (f"{self.name} uses {self.action_power} to activate their {self.power}.")
     
     def set_quality(self, quality):
         self.quality = quality
@@ -15,24 +19,23 @@ class Superhero:
     def printname(self):
         print(f"Superhero name: {self.name}")
 
-hero1 = Superhero("Superman", "flight")
-hero2 = Superhero("Batman", "stealth")
-hero3 = Superhero("WonderWoman", "super strength")
+    def printpower(self):
+        print(f"Superhero power: {self.power}")
+    
+    def printorigin(self):
+        print(f"Superhero origin: {self.origin}")
+    
+    def printfrends(self):
+        print(f"Superhero frends: {self.frends}")
+
+
+hero1 = Superhero("Superman", "flight", "Krypton", "SuperWoman", "heart")
+hero2 = Superhero("Batman", "stealth" , "Earth", "Superman", "hand")
+hero3 = Superhero("WonderWoman", "super strength" , "Themyscira", "Batman", "brain")
 
 hero1.use_power()
 hero2.use_power()
 hero3.use_power()
 
-hero1.set_quality("bravery")
-hero2.set_quality("intelligence")
-hero3.set_quality("wisdom")
-
-hero1.use_quality()
-hero2.use_quality()
-hero3.use_quality()
-
-hero1.printname()
-hero2.printname()
-hero3.printname()
 
 
